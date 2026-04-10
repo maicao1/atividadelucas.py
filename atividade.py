39d58e5b1182db6da91d005e0a88bcf3071afc14
maicao1 <maicon.carolino.campos@escola.pr.gov.br>
2026-04-10 11:41:44 -0300

    Add files via upload

---
+++ atividade.py
@@ -0,0 +1,21 @@
+
+#atividade 1
+
+tri1 = 50
+tri2 = 60
+tri3 = 70
+resultado = tri1 + tri2 + tri3
+i = "vinicius"
+
+if  resultado >= 180:
+    print("A pontuacao do aluno "+i+" foi de "+str(resultado)+" entao foi aprovado")
+else:
+    print("A pontuacao do aluno "+i+" foi de "+str(resultado)+" entao foi reprovado")
+
+    #/////////////////////////////////////////////////////////////
+
+    #atividade 2
+
+nucleotídeo = ""
+
+if (nucleotídeo != )
