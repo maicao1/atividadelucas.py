36e88d399feb3cf1c43cfa83b5d1013a5c987f31
maicao1 <maicon.carolino.campos@escola.pr.gov.br>
2026-04-14 20:17:28 -0300

    Update atividade.py

--- atividade.py
+++ atividade.py
@@ -12,13 +12,7 @@ if  resultado >= 180:
 else:
     print("A pontuacao do aluno "+i+" foi de "+str(resultado)+" entao foi reprovado")
 
-    #/////////////////////////////////////////////////////////////
-
-    #atividade 2
-
-nucleotídeo = ""
-
-if (nucleotídeo != )
+#/////////////////////////////////////////////////////////////
 
 #atividade 2
 
