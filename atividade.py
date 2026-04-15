b09282deadbfc25149525df2eb971cd525a9e9e1
maicao1 <maicon.carolino.campos@escola.pr.gov.br>
2026-04-14 21:22:23 -0300

    Update atividade.py

--- atividade.py
+++ atividade.py
@@ -33,3 +33,43 @@ elif digit == "G":
 else:
     print("nucleotídeo invalido")
   
+#//////////////////////////////////////////////////////////
+
+#atividade 3
+
+print("informe dois numeros da sua escolha")
+
+num1 = int(input("informe o primeiro numero da sua escolha:"))
+num2 = int(input("informe o segundo numero da sua escolha:"))
+
+num = print("escolha uma das opções:")
+
+print("1. Some de 2 números")          
+print("2. Diferença entre os 2 números (maior pelo menor)")          
+print("3. Produto entre 2 números.")          
+print("4. Divisão entre 2 números (o denominador não pode ser zero)")    
+
+escolha = input("DIGITE O NUMERO DA OPCAO ESCOLHIDA AQUI:")
+
+if escolha == "1":
+    resultado = num1 + num2
+    print("O resultado foi", resultado)
+    
+elif escolha == "2":
+    resultado = num1 > num2
+    print("O resultado foi", resultado)
+    
+elif escolha == "3":
+    resultado = num1 * num2
+    print("O resultado foi", resultado)
+    
+elif escolha == "4":
+    if num2 != 0:
+     resultado = num1 / num2
+     print("O resultado foi", resultado)
+    else:
+     print("não é possível dividir por zero!")
+
+else:
+    print("Opção inválida!")
+
